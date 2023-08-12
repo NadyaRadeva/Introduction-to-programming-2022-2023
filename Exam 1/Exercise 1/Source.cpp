10d8538f0e2bb22720b301387da431ea28ea352a
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-08-12 09:52:39 +0300

    Add files via upload

---
+++ Exam 1/Exercise 1/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<cmath>
+
+//!Решето на Ератостен!
+
+const int MAX_SIZE = 1024;
+
+int main() {
+	std::cout << "Enter a number: ";
+	int numb;
+	std::cin >> numb;
+	if (!std::cin) {
+		std::cout << "Error! Wrong input!" << std::endl;
+		return 1;
+	}
+	if (numb <= 0 || numb > 1024) {
+		std::cout << "Invalid input!" << std::endl;
+		return 1;
+	}
+
+	bool isPrime[MAX_SIZE] = { false,false };
+
+	for (size_t i = 2; i <= numb; ++i) {
+		isPrime[i] = true;
+	}
+
+	for (size_t i = 2; i < sqrt(numb); ++i) {
+		if (isPrime[i]) {
+			for (size_t j = i * 2; j <= numb; j += i) {
+				isPrime[i] = false;
+			}
+		}
+	}
+
+	int primeNumbs[MAX_SIZE], j = 0;
+	for (size_t p = 2; p <= numb; p++) {
+		if (isPrime[p]) {
+			primeNumbs[j++] = p;
+		}
+	}
+
+	for (size_t i = 0; i < j; ++i) {
+		if (isPrime[i + 1]) {
+			std::cout << primeNumbs[i] << " ";
+		}
+	}
+	std::cout << std::endl;
+}
